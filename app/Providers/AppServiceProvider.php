6e27a140fbb57f7c47e8afe60657a1082c168aaf
Oscar Alvarenga <dev_deep0@proton.me>
2026-05-03 00:16:41 -0600

    Fix: Forzar HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- 1. IMPORTANTE: Agrega esta línea
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Agregamos esto para que en Producción use HTTPS
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
